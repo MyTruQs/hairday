1fe3920ba13a3b3d8ab0ff570e2c100dc05fafdf
MyTruQs <raulfernandes.rm@gmail.com>
2024-10-15 21:47:52 -0300

    manipulation forms

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,21 +1,53 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector("form");
+const clientName = document.getElementById("client");
 const selectedDate = document.getElementById("date");
 
-// Data atual para o input.
+// Data atual para formatar o input.
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
 
-// Carrega a data atual.
+// Carrega a data atual e define a data minima como sendo a data atual.
 selectedDate.value = inputToday;
-
-// Define a data minima como sendo a data atual.
 selectedDate.min = inputToday;
 
 form.onsubmit = (event) => {
     // Previne o comportamento padrão de carregar a pagina.
     event.preventDefault();
 
-    console.log("Formulário enviado!");
+    try {
+        // Recuperando o nome do cliente.
+        const name = clientName.value.trim();
+
+        if (!name) {
+            return alert("Informe o nome do cliente!");
+        }
+
+        // Recupera o horario selecionado.
+        const hourSelected = document.querySelector(".hour-selected");
+
+        if (!hourSelected) {
+            return alert("Selecione um horario!");
+        }
+
+        // Recupera somente a hora
+        const [hour] = hourSelected.innerText.split(":");
+
+        // Insere a hora na data
+        const when = dayjs(selectedDate.value).add(hour, "hour");
+        
+        // Gera um ID
+        const id = new Date().getTime();
+
+        console.log({
+            id,
+            name,
+            when
+        });
+
+    } catch (error) {
+        alert("Não foi possivel realizar o agendamento!!!")
+        console.log(error);
+    }
 
 }
